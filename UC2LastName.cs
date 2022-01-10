a9f5de89f5a3ec5fd81147a44cdaf87c705ed57e
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-01-10 11:09:45 +0530

    [add]Aaryan added last name program

---
+++ UC2LastName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace UserRegistration
+{
+    public class UC2LastName
+    {
+        public static string LAST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+
+        public bool ValidateFirstName(string lName)
+        {
+            return Regex.IsMatch(lName, LAST_NAME_REGEX);
+        }
+    }
+}
